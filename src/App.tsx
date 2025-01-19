6b24c029b64c31fb537598f2044679551c087e07
vongsapateasyrice <vongsapat.ban@easyrice.ai>
2025-01-19 15:16:39 +0700

    feat: v1.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Version 1.0.0</h1>
+      <h1>Version 1.0.1</h1>
 
       <div className="row">
         <a href="https://vitejs.dev" target="_blank">
