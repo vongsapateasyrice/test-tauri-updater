6a354112e48f2c76d9d79153a87992aca4d8d3d5
vongsapateasyrice <vongsapat.ban@easyrice.ai>
2025-01-19 15:44:18 +0700

    feat: add logger

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,14 +4,25 @@ use tauri_plugin_updater::UpdaterExt;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new()
+        .target(tauri_plugin_log::Target::new(
+        tauri_plugin_log::TargetKind::Stdout,
+        ))
+        .target(tauri_plugin_log::Target::new(
+        tauri_plugin_log::TargetKind::LogDir {
+          file_name: Some("logs".to_string()),
+          },
+        ))
+        .build())
         .setup(|app| {
             #[cfg(desktop)]
             let _ = app.handle().plugin(tauri_plugin_updater::Builder::new().build());
+            
             let handle = app.handle().clone();
             tauri::async_runtime::spawn(async move {
                 update(handle).await.unwrap();
             });
-            println!("meow2");
+            log::info!("meow2");
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![commands::greet, commands::add_number])
@@ -25,41 +36,39 @@ pub fn run() {
   async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
     if let Some(update) = app.updater()?.check().await? {
       let mut downloaded = 0;
-      println!("hasUpdate! :3");
-      println!("currentver: {}", update.current_version);
-      println!("ver: {}", update.version);
-      println!("sign: {}", update.signature);
+      log::info!("hasUpdate! :3");
+      log::info!("currentver: {}", update.current_version);
+      log::info!("ver: {}", update.version);
+      log::info!("sign: {}", update.signature);
   
       // alternatively we could also call update.download() and update.install() separately
       match update
         .download_and_install(
           |chunk_length, content_length| {
             downloaded += chunk_length;
-            println!("downloaded {downloaded} from {content_length:?}");
+            log::info!("downloaded {downloaded} from {content_length:?}");
           },
           || {
-            println!("download finished");
+            log::info!("download finished");
           },
 
         )
         .await {
           Ok(_) => {
             // Handle successful download
-            println!("Download successful")
+            log::info!("Download successful")
             // Use data here...
           },
           Err(e) => {
             // Handle error case
-            println!("Download failed: {}", e);
+            log::info!("Download failed: {}", e);
             // Additional error handling...
           }
         }
-      
-      println!("update downloaded :3");
-      println!("update installed");
-      app.restart();
+      log::info!("update installed");
+      // app.restart();
     } else {
-      println!("no update aw :(")
+      log::info!("no update aw :(")
     }
   
     Ok(())
